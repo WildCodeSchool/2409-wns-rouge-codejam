4031a9b7b9139f3ba59bc0646292f3638d409484
Denis Dezest <denis.dezest@gmail.com>
2025-04-10 15:29:25 +0200

    fix: remove unnecessary test

--- frontend/src/__tests__/passwordVisibility.test.tsx
+++ frontend/src/__tests__/passwordVisibility.test.tsx
@@ -4,7 +4,6 @@ import PasswordVisibiltyInput from '@/shared/components/PasswordVisibiltyInput'
 
 describe('Password visibility input', () => {
   it('should switch the visibilty of the password when we click on the eye', () => {
-    expect(1 + 1).toBe(2)
     render(<PasswordVisibiltyInput name="password" />)
     const button = screen.getByRole('button', { name: 'Show password' })
     const input = screen.getByTestId('custom-password-field')
