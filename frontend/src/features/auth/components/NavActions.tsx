72b44ce683a18c40c1eeb112129e9453d84ead6f
Denis Dezest <denis.dezest@gmail.com>
2025-05-13 14:35:03 +0200

    feat: make user's thumbnail a popover to display a submenu on click

--- frontend/src/features/auth/components/NavActions.tsx
+++ frontend/src/features/auth/components/NavActions.tsx
@@ -1,10 +1,8 @@
-import { useMutation, useSuspenseQuery } from '@apollo/client'
+import { useSuspenseQuery } from '@apollo/client'
 import { useState } from 'react'
-import { toast } from 'sonner'
 import UserInfo from '@/features/auth/components/UserInfo'
 import SignInForm from '@/features/auth/components/SignInForm'
 import SignUpForm from '@/features/auth/components/SignUpForm'
-import { LOGOUT } from '@/shared/api/logout'
 import { WHO_AM_I } from '@/shared/api/whoAmI'
 import Modal from '@/shared/components/Modal'
 import { Button } from '@/shared/components/ui/button'
@@ -12,7 +10,6 @@ import { Button } from '@/shared/components/ui/button'
 export default function NavActions() {
   const [isSignIn, setIsSignIn] = useState<boolean>()
   const { data: { whoAmI: user } = {} } = useSuspenseQuery(WHO_AM_I)
-  const [logout] = useMutation(LOGOUT)
 
   const isUserLoggedIn = user !== null && user !== undefined
   const showModal = isSignIn !== undefined
@@ -21,34 +18,10 @@ export default function NavActions() {
     setIsSignIn(undefined)
   }
 
-  const handleLogout = async (_e: React.MouseEvent<HTMLButtonElement>) => {
-    try {
-      if (!user) return
-
-      const { data, errors } = await logout({
-        refetchQueries: [{ query: WHO_AM_I }],
-      })
-
-      if (errors !== undefined || !data?.logout) {
-        if (errors) console.error('Failed to sign out:', errors)
-        throw new Error('Failed to sign out!')
-      }
-
-      toast.success('Successful logout', {
-        description: '👋 Hope to see you soon!',
-      })
-    } catch (error: unknown) {
-      console.error(error)
-    }
-  }
-
   return (
     <div className="flex flex-row items-center gap-4">
       {isUserLoggedIn ? (
-        <>
-          <UserInfo user={user} />
-          <Button onClick={handleLogout}>Sign Out</Button>
-        </>
+        <UserInfo user={user} />
       ) : (
         <>
           <Button
