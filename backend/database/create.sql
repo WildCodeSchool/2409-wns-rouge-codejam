35acc64e343897d6ff6762338cafd0370bd41776
Denis Dezest <denis.dezest@gmail.com>
2025-06-13 10:52:37 +0200

    fix: enum type for `role` in dump file

--- backend/database/create.sql
+++ backend/database/create.sql
@@ -5,7 +5,7 @@ CREATE    TABLE "user" (
           "username" VARCHAR(32) UNIQUE,
           "email" VARCHAR(320) NOT NULL UNIQUE,
           "hashedPassword" VARCHAR(150) NOT NULL,
-          "role" UserRole NOT NULL DEFAULT 'user',
+          "role" "UserRole" NOT NULL DEFAULT 'user',
           "createdAt" TIMESTAMP NOT NULL,
           "updatedAt" TIMESTAMP NOT NULL
           );
