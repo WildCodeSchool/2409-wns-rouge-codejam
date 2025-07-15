9e4a9a7a650054729510995ddcc9bdbc1ab1e768
Denis Dezest <denis.dezest@gmail.com>
2025-07-15 16:37:13 +0200

    fix(#100): move toaster container to `App` to remove undesired bottom gap in the main layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 import { EditorLayout, MainLayout } from '@/shared/components/layouts'
+import { Toaster } from '@/shared/components/ui/sonner'
 import { HomePage } from '@/shared/pages'
 
 const client = new ApolloClient({
@@ -28,6 +29,7 @@ export default function App() {
             <Route path="*" Component={HomePage} />
           </Route>
         </Routes>
+        <Toaster richColors />
       </BrowserRouter>
     </ApolloProvider>
   )
--- frontend/src/shared/components/layouts/MainLayout.tsx
+++ frontend/src/shared/components/layouts/MainLayout.tsx
@@ -1,15 +1,14 @@
 import { Outlet } from 'react-router-dom'
-import NavBar from '../NavBar'
-import { Toaster } from '../ui/sonner'
+
+import NavBar from '@/shared/components/NavBar'
 
 export default function MainLayout() {
   return (
-    <div className="grid h-dvh grid-rows-[auto_1fr] gap-4 border border-solid border-red-500 p-4">
+    <div className="grid h-dvh grid-rows-[auto_1fr] gap-4 p-4">
       <NavBar />
       <main>
         <Outlet />
       </main>
-      <Toaster richColors />
     </div>
   )
 }
